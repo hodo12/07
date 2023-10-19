3b6551b619e129d2a87ee48b9b99e550a98219a5
sungha <choish0717@sookmyung.ac.kr>
2023-10-19 16:26:09 +0900

    06-1

--- main.c
+++ main.c
@@ -3,13 +3,13 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-static int all_files;
-exturn void sub();
+int all_files;
+extern void sub();
 
 int main(int argc, char *argv[])
 {
    sub();
-   ptinf("%d\n", all_files);
+   printf("%d\n", all_files);
    return 0;
 }
 
